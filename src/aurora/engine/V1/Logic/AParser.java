e0261445adbeea0554e0a5da6fe68f4fe88cde14
Marius PrideRage <djtool@hotmail.de>
2012-12-24 18:11:29 +0100

    Comments added

--- src/aurora/engine/V1/Logic/AParser.java
+++ src/aurora/engine/V1/Logic/AParser.java
@@ -7,29 +7,69 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+/**
+ * .------------------------------------------------------------------------.
+ * | AParser
+ * .------------------------------------------------------------------------.
+ * |
+ * | This class reads a File and stores the content of this file line by line
+ * | in an ArrayList.
+ * | 
+ * |
+ * .........................................................................
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ * 
+ */
+
 public class AParser {
 
+	/**
+	 * Content of File
+	 */
 	private ArrayList<String> parrsedArray;
+	
+	/**
+	 * File Manager
+	 */
 	private AFileManager mngr;
 
-	/**
-	 * Interact with file system to get required data
-	 * 
-	 * @param RootPath
-	 *            path to the directory containing the file(s) you want to parse
-	 */
+	
+    /**
+     * .-----------------------------------------------------------------------.
+     * | AParser(String RootPath)
+     * .-----------------------------------------------------------------------.
+     * |
+     * | This constructor prepares the AFileManager to read the file from the
+     * | correct path.
+     * | It also ensures that the ArrayList is initialized.
+     * | 
+     * .........................................................................
+     *
+     * @param RootPath path to the directory containing the file(s) you want to parse
+     *
+     */
 	public AParser(String RootPath) {
 		this.mngr = new AFileManager(RootPath, true);
 		this.parrsedArray = new ArrayList<String>();
 	}
-
-	/**
-	 * Will Read Your File Line By Line and return a ArrayList Containing each
-	 * line
-	 * 
-	 * @param file
-	 * @throws IOException
-	 */
+	
+    /**
+     * .-----------------------------------------------------------------------.
+     * | parseFile(String fileName)
+     * .-----------------------------------------------------------------------.
+     * |
+     * | This function reades the given File in the directory specified in the
+     * | constructor AParser and stores the contents of the File in an ArrayList.
+     * | 
+     * | This function returns this ArrayList 
+     * | 
+     * .........................................................................
+     *
+     * @param fileName name of the File to be parsed
+     * @throws IOException
+     *
+     */
 	public ArrayList<String> parseFile(String fileName) throws IOException {
 		this.parrsedArray = new ArrayList<String>();
 		FileInputStream inFile = null;
