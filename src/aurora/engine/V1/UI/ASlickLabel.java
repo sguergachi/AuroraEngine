b5234cf5078e9c20fd7d18b99eaf3df2f15a77e1
Sammy <sguergachi@gmail.com>
2012-12-28 01:44:20 -0500

    AAnimate acceleration added

--- src/aurora/engine/V1/UI/ASlickLabel.java
+++ src/aurora/engine/V1/UI/ASlickLabel.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package aurora.engine.V1.UI;
 
 import java.awt.Graphics;
@@ -13,13 +12,10 @@ import javax.swing.JLabel;
 
 /**
  * A Better Rendered Label
+ * <p/>
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
-public class ASlickLabel extends JLabel{
-
-    public ASlickLabel(Icon image) {
-        super(image);
-    }
+public class ASlickLabel extends JLabel {
 
     public ASlickLabel(String text) {
         super(text + "  ");
@@ -29,36 +25,28 @@ public class ASlickLabel extends JLabel{
         super(text + "  ", horizontalAlignment);
     }
 
-    public ASlickLabel(Icon image, int horizontalAlignment) {
-        super(image, horizontalAlignment);
-    }
-
-    public ASlickLabel(String text, Icon icon, int horizontalAlignment) {
-        super(text + "  ", icon, horizontalAlignment);
-    }
-
     public ASlickLabel() {
     }
 
     @Override
     public void setText(String text) {
-        super.setText(text + "  " );
+        super.setText(text + "  ");
     }
 
-
-
-
-     @Override
+    @Override
     protected void paintComponent(Graphics g) {
 
         Graphics2D g2d = (Graphics2D) g;
 
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,
+                RenderingHints.VALUE_FRACTIONALMETRICS_ON);
 
         super.paintComponent(g2d);
     }
-
 }
