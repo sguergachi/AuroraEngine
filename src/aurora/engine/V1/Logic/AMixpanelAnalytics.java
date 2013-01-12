5f115b280bdc0f717c6fe5205f59d5519d54fe22
Sammy <sguergachi@gmail.com>
2013-01-12 03:43:23 -0500

    now using MAC address as Unique ID

--- src/aurora/engine/V1/Logic/AMixpanelAnalytics.java
+++ src/aurora/engine/V1/Logic/AMixpanelAnalytics.java
@@ -11,6 +11,11 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Queue;
 import java.util.UUID;
 import java.util.logging.Level;
@@ -31,6 +36,8 @@ public class AMixpanelAnalytics {
 
     private MixpanelAPI mixpanelAPI;
 
+    private String ID;
+
     public AMixpanelAnalytics(String Token) {
         PROJECT_TOKEN = Token;
         mixpanelAPI = new MixpanelAPI();
@@ -39,7 +46,47 @@ public class AMixpanelAnalytics {
     public void sendEvent(String Event) {
 
         MessageBuilder builder = new MessageBuilder(PROJECT_TOKEN);
-        message = builder.event(getMotherboardSN(),
+        String computername = null;
+        try {
+            computername = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(AMixpanelAnalytics.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+
+
+
+        // Get Mac Address for Unique ID //
+        InetAddress ip;
+        try {
+
+            ip = InetAddress.getLocalHost();
+
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+
+            byte[] mac = network.getHardwareAddress();
+
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < mac.length; i++) {
+                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ?
+                        "-" : ""));
+            }
+
+            ID = sb.toString();
+
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+
+        } catch (SocketException e) {
+
+            e.printStackTrace();
+
+        }
+
+        // Build the Message to send //
+        message = builder.event(ID,
                 Event, null);
         delivery = new ClientDelivery();
         delivery.addMessage(message);
@@ -59,38 +106,4 @@ public class AMixpanelAnalytics {
         worker.startOnce();
 
     }
-
-    public static String getMotherboardSN() {
-  String result = "";
-    try {
-      File file = File.createTempFile("mb-sn",".vbs");
-      file.deleteOnExit();
-      FileWriter fw = new java.io.FileWriter(file);
-
-      String vbs =
-         "Set objWMIService = GetObject(\"winmgmts:\\\\.\\root\\cimv2\")\n"
-        + "Set colItems = objWMIService.ExecQuery _ \n"
-        + "   (\"Select * from Win32_BaseBoard\") \n"
-        + "For Each objItem in colItems \n"
-        + "    Wscript.Echo objItem.SerialNumber \n"
-        + "    exit for  ' do the first cpu only! \n"
-        + "Next \n";
-
-      fw.write(vbs);
-      fw.close();
-      Process p = Runtime.getRuntime().exec("cscript //NoLogo " + file.getPath());
-      BufferedReader input =
-        new BufferedReader
-          (new InputStreamReader(p.getInputStream()));
-      String line;
-      while ((line = input.readLine()) != null) {
-         result += line;
-      }
-      input.close();
-    }
-    catch(Exception e){
-        e.printStackTrace();
-    }
-    return result.trim();
-  }
 }
