4cbe5b8d2921cd7c629f13c75961ab529f700242
Sammy <sguergachi@gmail.com>
2013-01-12 02:33:36 -0500

    unique ID from motherBoard wow!

--- src/aurora/engine/V1/Logic/AMixpanelAnalytics.java
+++ src/aurora/engine/V1/Logic/AMixpanelAnalytics.java
@@ -6,8 +6,13 @@ import aurora.engine.V1.Logic.mixpanelapi.MessageBuilder;
 import aurora.engine.V1.Logic.mixpanelapi.MixpanelAPI;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Queue;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -34,7 +39,7 @@ public class AMixpanelAnalytics {
     public void sendEvent(String Event) {
 
         MessageBuilder builder = new MessageBuilder(PROJECT_TOKEN);
-        message = builder.event(System.getProperty("user.name")+System.nanoTime(),
+        message = builder.event(getMotherboardSN(),
                 Event, null);
         delivery = new ClientDelivery();
         delivery.addMessage(message);
@@ -54,4 +59,38 @@ public class AMixpanelAnalytics {
         worker.startOnce();
 
     }
+
+    public static String getMotherboardSN() {
+  String result = "";
+    try {
+      File file = File.createTempFile("mb-sn",".vbs");
+      file.deleteOnExit();
+      FileWriter fw = new java.io.FileWriter(file);
+
+      String vbs =
+         "Set objWMIService = GetObject(\"winmgmts:\\\\.\\root\\cimv2\")\n"
+        + "Set colItems = objWMIService.ExecQuery _ \n"
+        + "   (\"Select * from Win32_BaseBoard\") \n"
+        + "For Each objItem in colItems \n"
+        + "    Wscript.Echo objItem.SerialNumber \n"
+        + "    exit for  ' do the first cpu only! \n"
+        + "Next \n";
+
+      fw.write(vbs);
+      fw.close();
+      Process p = Runtime.getRuntime().exec("cscript //NoLogo " + file.getPath());
+      BufferedReader input =
+        new BufferedReader
+          (new InputStreamReader(p.getInputStream()));
+      String line;
+      while ((line = input.readLine()) != null) {
+         result += line;
+      }
+      input.close();
+    }
+    catch(Exception e){
+        e.printStackTrace();
+    }
+    return result.trim();
+  }
 }
