058488f19ed3f2d462489cdbd87b44ef158770aa
Sammy <sguergachi@gmail.com>
2014-05-22 22:08:16 -0400

    fixes RSS

--- src/aurora/engine/V1/Logic/ARssReader.java
+++ src/aurora/engine/V1/Logic/ARssReader.java
@@ -9,14 +9,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
-
-import java.util.ArrayList;
-import java.util.List;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Characters;
 import javax.xml.stream.events.XMLEvent;
+import org.apache.log4j.Logger;
 
 /**
  * Version 2.1
@@ -25,6 +23,8 @@ import javax.xml.stream.events.XMLEvent;
  * @author Lars Vogel
  */
 public class ARssReader {
+
+    static final Logger logger = Logger.getLogger(ARssReader.class);
 
     /*
      * Stores an RSS feed
@@ -87,10 +87,12 @@ public class ARssReader {
         @Override
         public String toString() {
             return "Feed [copyright=" + copyright + ", description="
-                   + description
-                   + ", language=" + language + ", link=" + link + ", pubDate="
-                   + pubDate + ", title=" + title + "]";
+                           + description
+                           + ", language=" + language + ", link=" + link
+                           + ", pubDate="
+                           + pubDate + ", title=" + title + "]";
         }
+
     }
 
     /*
@@ -148,16 +150,15 @@ public class ARssReader {
             this.guid = guid;
         }
 
-        /* @Override
-         public String toString() {
-         return "FeedMessage [title=" + title + ", description=" + description
-         + ", link=" + link + ", author=" + author + ", guid=" + guid
-         + "]";
-         }*/
         @Override
         public String toString() {
-            return title;
+            return "FeedMessage [title=" + title + ", description="
+                           + description
+                           + ", link=" + link + ", author=" + author + ", guid="
+                           + guid
+                           + "]";
         }
+
     }
 
     public class RSSFeedParser {
@@ -192,13 +193,11 @@ public class ARssReader {
             }
         }
 
-        @SuppressWarnings("null")
         public Feed readFeed() {
             Feed feed = null;
             try {
-
                 boolean isFeedHeader = true;
-                // Set header values initially to the empty string
+                // Set header values intial to the empty string
                 String description = "";
                 String title = "";
                 String link = "";
@@ -214,73 +213,47 @@ public class ARssReader {
                 InputStream in = read();
                 XMLEventReader eventReader = inputFactory.createXMLEventReader(
                         in);
-                // Read the XML document
+                // read the XML document
                 while (eventReader.hasNext()) {
-
                     XMLEvent event = eventReader.nextEvent();
-
                     if (event.isStartElement()) {
-                        if (event.asStartElement().getName().getLocalPart()
-                            == (ITEM)) {
-                            if (isFeedHeader) {
-                                isFeedHeader = false;
-                                feed = new Feed(title, link, description,
-                                        language,
-                                        copyright, pubdate);
-                            }
-                            event = eventReader.nextEvent();
-                            continue;
-                        }
-
-                        if (event.asStartElement().getName().getLocalPart()
-                            == (TITLE)) {
-                            event = eventReader.nextEvent();
-                            title = event.asCharacters().getData();
-                            continue;
-                        }
-                        if (event.asStartElement().getName().getLocalPart()
-                            == (DESCRIPTION)) {
-                            event = eventReader.nextEvent();
-                            description = event.asCharacters().getData();
-                            continue;
-                        }
-
-                        if (event.asStartElement().getName().getLocalPart()
-                            == (LINK)) {
-                            event = eventReader.nextEvent();
-                            link = event.asCharacters().getData();
-                            continue;
-                        }
-
-                        if (event.asStartElement().getName().getLocalPart()
-                            == (GUID)) {
-                            event = eventReader.nextEvent();
-                            guid = event.asCharacters().getData();
-                            continue;
-                        }
-                        if (event.asStartElement().getName().getLocalPart()
-                            == (LANGUAGE)) {
-                            event = eventReader.nextEvent();
-                            language = event.asCharacters().getData();
-                            continue;
-                        }
-                        if (event.asStartElement().getName().getLocalPart()
-                            == (AUTHOR)) {
-                            event = eventReader.nextEvent();
-                            author = event.asCharacters().getData();
-                            continue;
-                        }
-                        if (event.asStartElement().getName().getLocalPart()
-                            == (PUB_DATE)) {
-                            event = eventReader.nextEvent();
-                            pubdate = event.asCharacters().getData();
-                            continue;
-                        }
-                        if (event.asStartElement().getName().getLocalPart()
-                            == (COPYRIGHT)) {
-                            event = eventReader.nextEvent();
-                            copyright = event.asCharacters().getData();
-                            continue;
+                        String localPart = event.asStartElement().getName()
+                                .getLocalPart();
+                        switch (localPart) {
+                            case ITEM:
+                                if (isFeedHeader) {
+                                    isFeedHeader = false;
+                                    feed = new Feed(title, link, description,
+                                                    language,
+                                                    copyright, pubdate);
+                                }
+                                event = eventReader.nextEvent();
+                                break;
+                            case TITLE:
+                                title = getCharacterData(event, eventReader);
+                                break;
+                            case DESCRIPTION:
+                                description = getCharacterData(event,
+                                                               eventReader);
+                                break;
+                            case LINK:
+                                link = getCharacterData(event, eventReader);
+                                break;
+                            case GUID:
+                                guid = getCharacterData(event, eventReader);
+                                break;
+                            case LANGUAGE:
+                                language = getCharacterData(event, eventReader);
+                                break;
+                            case AUTHOR:
+                                author = getCharacterData(event, eventReader);
+                                break;
+                            case PUB_DATE:
+                                pubdate = getCharacterData(event, eventReader);
+                                break;
+                            case COPYRIGHT:
+                                copyright = getCharacterData(event, eventReader);
+                                break;
                         }
                     } else if (event.isEndElement()) {
                         if (event.asEndElement().getName().getLocalPart()
@@ -301,7 +274,17 @@ public class ARssReader {
                 throw new RuntimeException(e);
             }
             return feed;
+        }
 
+        private String getCharacterData(XMLEvent event,
+                                        XMLEventReader eventReader)
+                throws XMLStreamException {
+            String result = "";
+            event = eventReader.nextEvent();
+            if (event instanceof Characters) {
+                result = event.asCharacters().getData();
+            }
+            return result;
         }
 
         private InputStream read() {
